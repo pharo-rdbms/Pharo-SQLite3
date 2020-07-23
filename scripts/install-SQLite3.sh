972f4403d9016914b3bf7fd8f916ef8e65a81ecb
Gabriel Omar Cotelli <g.cotelli@gmail.com>
2020-07-23 10:04:11 -0300

    Pin sqlite3 version to install

--- scripts/install-SQLite3.sh
+++ scripts/install-SQLite3.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-sudo apt-get --assume-yes --no-install-recommends install sqlite3
+sudo apt-get --assume-yes --no-install-recommends install sqlite3=3.32.3-1
 sqlite3 -version
 ln -s /usr/lib/x86_64-linux-gnu/libsqlite3.so.0 libsqlite3.so
